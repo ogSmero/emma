06ae9215fa540b7cdf38545a040c09277e89a9f7
ogSmero <sociopath.tony@gmail.com>
2022-03-25 12:22:45 +0200

    new
    my base start for project
    and
    +- design

---
+++ src/App/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return(
+        <div className='application'>
+            new Application/
+        </div>
+    );
+}
+
+export default App;
